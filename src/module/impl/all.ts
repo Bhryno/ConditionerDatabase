a290a29484f19306cacd74e81d5318c8eaa243f8
HairConditioner <bryanrplee@gmail.com>
2023-01-01 13:36:18 +1100

    add module

--- src/module/impl/all.ts
+++ src/module/impl/all.ts
@@ -2,16 +2,13 @@ import { Database } from "better-sqlite3"
 import { ModuleParameters } from "../moduleFactory"
 
 export default function all(database: Database, parameters: ModuleParameters, table: string): { ID: any, data: any }[] {
-
     let currentValues = [...database.prepare(`SELECT * FROM ${table}`).iterate()]
 
     return currentValues.map( r => (
-
         {
             ID: r.ID,
             data: JSON.parse(r.json)
         }
-
     ))
 
 }
--- src/module/moduleFactory.ts
+++ src/module/moduleFactory.ts
@@ -1,39 +1,34 @@
 import { Database } from "better-sqlite3"
 
+import add from "./impl/add"
 import all from "./impl/all"
 
 const safeEcho = false
 
 const modules = {
+    add,
     all
 }
 
 export type ModuleOptions = {
-
     table?: string,
     target?: string,
     subKeys?: boolean
-
 }
 
 type ArbitrateModuleParameters = {
-
     id?: string,
     options?: ModuleOptions,
     data?: any
-
 }
 
 export type ModuleParameters = {
-
     id?: string,
     options: ModuleOptions,
     data?: any
-
 }
 
 export function arbitrate(database: Database, module: keyof typeof modules, _parameters: ArbitrateModuleParameters, table?: string): any {
-    
     let parameters: ModuleParameters = { ...(_parameters || {}), options: {} }
 
     table = table || parameters.options?.table || "json"
@@ -41,172 +36,119 @@ export function arbitrate(database: Database, module: keyof typeof modules, _par
     database.prepare(`CREATE TABLE IF NOT EXISTS ${table} (ID TEXT, json TEXT)`).run()
 
     if (parameters.options.target && parameters.options.target.includes(".") && parameters.options.subKeys !== false) {
-
         parameters.options.target = parameters.options.target.slice(1)
-
     }
 
     if (parameters.data && parameters.data === Infinity) {
-
         throw new TypeError(`Cannot set value to infinity -> [${table}/${parameters.id}]`)
-
     }
 
     if (parameters.id && parameters.id.includes(".") && parameters.options.subKeys !== false) {
-
         let split = parameters.id.split(".")
 
         parameters.id = split.shift()
         parameters.options.target = split.join(".")
-
     }
 
     return modules[module](database, parameters, table)
-
 }
 
 export function getSafe(object: any, path: string): undefined | any {
-
     if (safeEcho) {
-
         console.log(`<GET SAFE> :: IN:`, object, `, PATH:`, path)
-
     }
 
     let parent = object
 
     for (let subPath of path.split(".")) {
-
         if (!parent[subPath]) {
-
             return undefined
-
         }
-
         parent = parent[subPath]
-
     }
 
     if (safeEcho) {
-
         console.log(`<GET SAFE RETURN> :: IN:`, object, `, PATH:`, path, `, VALUE:`, parent)
-
     }
 
     return parent
-
 }
 
 export function setSafe(object: any, path: string, value: any) {
-
     if (safeEcho) {
-
         console.log(`<SET SAFE> :: IN:`, object, `, PATH:`, path, `, VALUE:`, value)
-
     }
 
     let parent = object
     const splits = path.split(".")
 
     for (let i = 0; i < splits.length; i++) {
-
         let subPath = splits[i]
 
         if (parent[subPath] === undefined) {
-
             parent[subPath] = {}
-
         }
 
         if (i === splits.length - 1) {
-
             parent[subPath] = value
-
-        }
-        else {
-
+        } else {
             parent = parent[subPath]
-
         }
 
     }
 
     return object
-
 }
 
 export function deleteSafe(object: any, path: string): object {
-    
     if (safeEcho) {
-
         console.log(`<DELETE SAFE> :: IN:`, object, `, PATH:`, path)
-
     }
 
     let parent = object
     const splits = path.split(".")
 
     for (let i = 0; i < splits.length; i++) {
-        
         let subPath = splits[i]
 
         if (parent[subPath] === undefined) {
-
             parent[subPath] = {}
-
         }
 
         if (i === splits.length - 1) {
-
             delete parent[subPath]
-
-        }
-        else {
-
+        } else {
             parent = parent[subPath]
-
         }
-
     }
 
     return object
-
 }
 
 export function getValue(database: Database, parameters: ModuleParameters, table: string) {
-
     let currentValue = database.prepare(`SELECT * FROM ${table} WHERE ID = (?)`).get(parameters.id)
 
     if (currentValue === undefined) {
-
         database.prepare(`INSERT INTO ${table} (ID, JSON) VALUES (?, ?)`).run(parameters.id, "{}")
-
     }
 
     return currentValue
-
 }
 
 export function setValue(database: Database, parameters: ModuleParameters, table: string) {
-    
     database.prepare(`UPDATE ${table} SET json = (?) WHERE ID = (?)`).run(parameters.data, parameters.id)
 
     let newValue = database.prepare(`SELECT * FROM ${table} WHERE ID  = (?)`).get(parameters.id).json
 
     if (newValue === "{}") {
-
         return null
-
     }
 
     newValue = JSON.parse(newValue)
 
-    if (parameters .options.target && parameters.options.subKeys !== false) {
-
+    if (parameters.options.target && parameters.options.subKeys !== false) {
         return getSafe(newValue, parameters.options.target)
-
     }
 
     return newValue
-
 }
